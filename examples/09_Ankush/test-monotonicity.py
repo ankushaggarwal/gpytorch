5e8a44002172a62d3587ab4e243cc0f9528abfab
Ankush Aggarwal <ankush_aggarwal2@yahoo.com>
2021-06-04 12:46:05 +0100

    Added a simple example for testing monotonicity

---
+++ examples/09_Ankush/test-monotonicity.py
@@ -0,0 +1,87 @@
+import math
+import numpy as np
+import torch
+import sys
+from os.path import dirname, abspath
+sys.path.insert(0,dirname(dirname(dirname(abspath(__file__)))))
+import gpytorch
+from matplotlib import pyplot as plt
+
+# Training data is 100 points in [0,1] inclusive regularly spaced
+train_x = torch.linspace(0, 1, 100)
+# True function is sin(2*pi*x) with Gaussian noise
+train_y = torch.sin(train_x * (0.7* math.pi)) + torch.randn(train_x.size()) * math.sqrt(0.04)
+
+# We will use the simplest form of GP model, exact inference
+class ExactGPModel(gpytorch.models.ExactGP):
+    def __init__(self, train_x, train_y, likelihood):
+        super(ExactGPModel, self).__init__(train_x, train_y, likelihood)
+        self.mean_module = gpytorch.means.ConstantMean()
+        self.covar_module = gpytorch.kernels.ScaleKernel(gpytorch.kernels.RBFKernel())
+
+    def forward(self, x):
+        mean_x = self.mean_module(x)
+        covar_x = self.covar_module(x)
+        return gpytorch.distributions.MultivariateNormal(mean_x, covar_x)
+
+# initialize likelihood and model
+likelihood = gpytorch.likelihoods.GaussianLikelihood()
+model = ExactGPModel(train_x, train_y, likelihood)
+
+training_iter = 50
+
+# Find optimal model hyperparameters
+model.train()
+likelihood.train()
+
+# Use the adam optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=0.1)  # Includes GaussianLikelihood parameters
+
+# "Loss" for GPs - the marginal log likelihood
+mll = gpytorch.mlls.ExactMarginalLogLikelihood(likelihood, model)
+
+for i in range(training_iter):
+    # Zero gradients from previous iteration
+    optimizer.zero_grad()
+    # Output from model
+    output = model(train_x)
+    # Calc loss and backprop gradients
+    loss = -mll(output, train_y)
+    loss.backward()
+    print('Iter %d/%d - Loss: %.3f   lengthscale: %.3f   noise: %.3f' % (
+        i + 1, training_iter, loss.item(),
+        model.covar_module.base_kernel.lengthscale.item(),
+        model.likelihood.noise.item()
+    ))
+    optimizer.step()
+
+# Get into evaluation (predictive posterior) mode
+model.eval()
+likelihood.eval()
+
+# Test points are regularly spaced along [0,1]
+# Make predictions by feeding model through likelihood
+with gpytorch.settings.fast_pred_var():
+    test_x = torch.linspace(0, 1, 51,requires_grad=True)
+    observed_pred = likelihood(model(test_x))
+
+with gpytorch.settings.fast_pred_var():
+    # Initialize plot
+    f, (ax, ax2) = plt.subplots(2, 1, figsize=(4, 3))
+
+    # Get upper and lower confidence bounds
+    lower, upper = observed_pred.confidence_region()
+    # Plot training data as black stars
+    ax.plot(train_x.numpy(), train_y.numpy(), 'k*')
+    # Plot predictive means as blue line
+    ax.plot(test_x.detach().numpy(), observed_pred.mean.detach().numpy(), 'b')
+    # Shade between the lower and upper confidence bounds
+    ax.fill_between(test_x.detach().numpy(), lower.detach().numpy(), upper.detach().numpy(), alpha=0.5)
+    ax.set_ylim([-3, 3])
+    ax.legend(['Observed Data', 'Mean', 'Confidence'])
+
+    ax2.plot(test_x.detach().numpy(),
+            torch.autograd.grad(observed_pred.mean.sum(), test_x)[0].detach().numpy())
+
+plt.show()
+
